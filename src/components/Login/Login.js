2707415d688d2e41bad12b14b990b8bcf34a6892
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-03-17 23:34:17 +0600

    re-check

---
+++ src/components/Login/Login.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
+
+const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+
+    const handlegoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email };
+                setLoggedInUser(signedInUser);
+                history.replace(from);
+            }).catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+            });
+    }
+
+    return (
+        <div>
+            <h1>This is Login</h1>
+            <button onClick={handlegoogleSignIn}>Google Sign in</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAxghvXBriQr2fP9kHIIcBY2U7L3D1xSr0",
+    authDomain: "burj-al-arab-19577.firebaseapp.com",
+    projectId: "burj-al-arab-19577",
+    storageBucket: "burj-al-arab-19577.appspot.com",
+    messagingSenderId: "310988169704",
+    appId: "1:310988169704:web:8e963b9d9172b73f8b845d"
+};
+export default firebaseConfig;
